82c9e2de86213aba49638751cfef81c706c4eb46
Lasha Lezhava <lashalezhava@gmail.com>
2018-11-07 21:17:08 +0400

    switched to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { Route, Switch, HashRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./state/store/configureStore";
 import styles from "./app.module.css"
